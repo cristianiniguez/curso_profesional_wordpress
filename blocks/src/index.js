dbadd8b91856b1e4cc931afe395816b4f2b3c0a5
cristianiniguez <felizpc1@gmail.com>
2021-06-13 15:20:18 -0400

    utilizar bloques: componentes de wordpress

--- blocks/src/index.js
+++ blocks/src/index.js
@@ -1,10 +1,30 @@
 import { registerBlockType } from '@wordpress/blocks';
+import { TextControl } from '@wordpress/components';
 
 registerBlockType('pg/basic', {
   title: 'Basic Block',
   description: 'Este es nuestro primer bloque',
   icon: 'smiley',
   category: 'layout',
-  edit: () => <h2>Hello World</h2>,
-  save: () => <h2>Hello World</h2>,
+  attributes: {
+    content: {
+      type: 'string',
+      default: 'Hello World',
+    },
+  },
+  edit: (props) => {
+    const {
+      attributes: { content },
+      setAttributes,
+      className,
+      isSelected,
+    } = props;
+
+    const handleOnChangeInput = (newContent) => {
+      setAttributes({ content: newContent });
+    };
+
+    return <TextControl label='Complete el campo' value={content} onChange={handleOnChangeInput} />;
+  },
+  save: (props) => <h2>{props.attributes.content}</h2>,
 });
